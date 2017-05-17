45e01f290bc79a997bc97e9bfb8dbef2d2615224
LouayNabil <louaynabil@gmail.com>
2017-05-17 12:09:48 +0200

    ad projekt Day4

---
+++ ProgramDay4_Exercise1/TrekantClass.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramDay4_Exercise1
+{
+
+    //SKAL RETTES til den er ikke færdig!!
+    class TrekantClass
+    {
+        public int Grundlinje { get; private set; }
+        public int Højde { get; private set; }
+
+        private int areal;
+
+        public int Areal
+        {
+            get
+            {
+                areal = this.Grundlinje * this.Højde * 0.5;
+                return areal;
+            }
+            //set { Areal = value; }
+        }
+
+        //Custom constructor
+        public double TrekantClass(int int_Grundlinje, int int_Højde)
+        {
+            this.Grundlinje = int_Grundlinje;
+            this.Højde = int_Højde;
+        }
+    }
+}
