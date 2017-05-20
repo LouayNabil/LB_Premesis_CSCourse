1fa639d40435a026ecb7f74c9ca1b2d8388be86f
Louay Nabil Kamel <loka@lbforsikring.onmicrosoft.com>
2017-05-20 15:30:57 +0200

    Complete Day3Exe1

--- Day3_Exercise1/ProgramDay3Exe1.cs
+++ Day3_Exercise1/ProgramDay3Exe1.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace Day3_Exercise1
 {
     class ProgramDay3Exe1
-    {
+    {//Små metoder - Status=OK
         static void Main(string[] args)
         {
             int int_a = 10;
@@ -15,14 +15,15 @@ namespace Day3_Exercise1
             int Int_Radius = 5;
             double double_beløb = 5000;
             double double_momsPct = 0.25;
+            int[] månedsløn = { 3, 5, 7, 9 };
             //Mangler denne opgave: double månedsløn[] = { 1500, 2500, 3500, 4500 };
 
-            Console.WriteLine("Lægsammen: " + Lægsammen(int_a, int_b));
-            Console.WriteLine("Areal er:  " + BerenAreal(Int_Radius));
+            System.Console.WriteLine("Lægsammen: " + Lægsammen(10,20));// (int_a, int_b));
+            System.Console.WriteLine("Areal er:  " + BerenAreal(5));// (Int_Radius));
             Udskriv();
-            Console.WriteLine("moms er : "+ BeregnMoms(double_beløb, double_momsPct));
-           //Mangler denne opgave:  Console.WriteLine("Gennemsnit:  " + Gennemsnit (månedsløn));
-
+            Udskriv("Udskrivelse af: Dette er en test");
+            System.Console.WriteLine("moms er : " + BeregnMoms(double_beløb, double_momsPct));
+            System.Console.WriteLine("Gennemsnit:  " + Gennemsnit(månedsløn));
             System.Console.ReadKey();
         }
         static int Lægsammen(int a, int b)
@@ -39,11 +40,23 @@ namespace Day3_Exercise1
         {
             Console.WriteLine(" ");
         }
+        static void Udskriv(string str)
+        {
+            Console.WriteLine(str);
+        }
         static double BeregnMoms(double double_beløb, double double_momsPct)
         {
             double res4;
-            return (double_beløb* double_momsPct);
+            return (double_beløb * double_momsPct);
         }
-        //Mangler denne opgave:  static double Gennemsnit(int[] månedsløn)
+        static double Gennemsnit(int[] månedsløn)
+        {
+            double sum = 0;
+            for (int i = 0; i < månedsløn.Length; i++)
+            {
+                sum += månedsløn[i];
+            }
+            return (sum / månedsløn.Length);
+        }
     }
 }
