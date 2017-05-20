666f9579b5548ee9d5e08a76eb5ac80df97ced0f
Louay Nabil Kamel <loka@lbforsikring.onmicrosoft.com>
2017-05-20 21:39:47 +0200

    Day3 Exercise3 complete

---
+++ Day3_Exercise2/ProgramDay3Exe2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3_Exercise2
+{//Overload: status OK
+    class ProgramDay3Exe2
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("First Beregn: " + Beregn(3,5));
+            System.Console.WriteLine("First Beregn: " + Beregn(3, 5, 7));
+            System.Console.WriteLine("First Beregn: " + Beregn(3, 5, 7, 9));
+            System.Console.ReadKey();
+        }
+
+        public static double Beregn(int x, int y)
+        {
+            return (x + y);
+        }
+        public static double Beregn(int x, int y, int z)
+        {
+            return (x + y + z);
+        }
+        public static double Beregn(int x, int y, int z, int m)
+        {
+            return (x + y + z + m);
+        }
+    }
+}
---
+++ Day3_Exercise3/ProgramDay3Exe3.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3_Exercise3
+{ //Beregn array - status:?
+    class ProgramDay3Exe3
+    {
+        static void Main(string[] args)
+        {
+            int[] MyArray = { 1, 2, 3, 4, 5 };
+            int[] MyArray2 = { 1, 2, 3, 4, 5 };
+            var res = "";
+            System.Console.WriteLine("Gennemsnit som Day3Exe1, hvor det´r returneres KUN EN VÆRDI:  " + BeregnOgSorterArray(MyArray));
+            System.Console.WriteLine("Gennemsnit, hv:  " + BeregnOgSorterArray2(MyArray2));
+            System.Console.ReadKey();  
+
+        }
+        //static double Gennemsnit(int[] månedsløn)
+
+        static double BeregnOgSorterArray(int[] MyArray)
+        {
+            double sum = 0;
+            for (int i = 0; i < MyArray.Length; i++) { sum += MyArray[i]; }
+            return sum;
+        }
+        public static ArrayResultat BeregnOgSorterArray2(int[] MyArray2)
+        {
+            ArrayResultat MyStruct;
+            MyStruct.sum = 0;
+            MyStruct.gennemsnit = 0;
+            foreach (var item in MyArray2) { MyStruct.sum += item; }
+            MyStruct.gennemsnit = (MyStruct.sum / MyArray2.Length);
+            Array.Sort(MyArray2);
+            return MyStruct;
+        }
+        /*    public double struct ArrayResultat;
+        public double sum;
+        public double Gennemsnit;
+        //System.Array.
+        //Array.sort
+        return int f//ArrayResultat;
+    */
+    }
+
+    struct ArrayResultat
+    {
+        public double sum;
+        public double gennemsnit;
+    }
+
+}
+
