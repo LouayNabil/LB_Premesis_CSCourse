b28440b4407f5ca07d15211487f2c081382af2b7
Louay Nabil Kamel <loka@lbforsikring.onmicrosoft.com>
2017-05-28 19:25:00 +0200

    add: Day3_Exercise5

---
+++ Day3_Exercise5/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3_Exercise5
+{//Rekursion Status: ?
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
