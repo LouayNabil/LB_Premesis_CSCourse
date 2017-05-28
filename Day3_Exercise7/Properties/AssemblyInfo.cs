fae4e9280700a2104ed917636e9eb75462790282
Louay Nabil Kamel <loka@lbforsikring.onmicrosoft.com>
2017-05-28 18:42:13 +0200

    Rename folder in  day3_Exe6&7 in Win Explorer

--- Day3_Exercise7/Properties/AssemblyInfo.cs
+++ Day3_Exercise7/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Day3_Exercise5")]
+[assembly: AssemblyTitle("Day3_Exercise6")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Day3_Exercise5")]
+[assembly: AssemblyProduct("Day3_Exercise6")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("24ca4f23-875b-4fdb-b865-535654e50212")]
+[assembly: Guid("8615a450-f3b7-41f0-b1f0-0915357a852e")]
 
 // Version information for an assembly consists of the following four values:
 //
