eca517b60b037c9fac84d2ff5061a5edc8434610
Louay Nabil Kamel <loka@lbforsikring.onmicrosoft.com>
2017-06-04 19:56:11 +0200

    --

---
+++ Day4_Exercise1/ProgramDay4_Exercise1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day4_Exercise1
+{//Egenskaber - Trekant class. Status: Done
+    class ProgramDay4_Exercise1
+    {
+        static void Main(string[] args)
+        {
+            Trekant_Class inst = new Trekant_Class(4,5);
+            Console.WriteLine("Trekan areal er: " + inst.Areal);
+            System.Console.ReadKey();
+        }
+
+    }
+}
---
+++ Day4_Exercise1/Trekant_Class.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day4_Exercise1
+{
+    class Trekant_Class
+    {
+        public int Grundlinje { get; private set; }
+        public int Højde { get; private set; }
+        public double Areal {
+            get {
+                return this.Grundlinje * this.Højde * 0.5;
+            }
+            set { }
+        }
+        public Trekant_Class(int Grundlinje, int Højde)
+        {
+            this.Grundlinje = Grundlinje;
+            this.Højde = Højde;
+            //Grundlinje*Højde*0.5
+        }
+
+    }
+}
