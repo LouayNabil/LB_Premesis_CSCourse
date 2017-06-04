95f7cf15746c0e71a6ec6e9ce986992c57c705f4
Louay Nabil Kamel <loka@lbforsikring.onmicrosoft.com>
2017-06-04 20:42:20 +0200

    update Day4_Exercise2

---
+++ Day4_Exercise2/ArealBeregningerClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramDay4_Exercise2
+{
+    class ArealBeregningerClass
+    {//•	
+        public static double BeregnArealFirkant(double højde, double brede)
+        { return højde * brede; }//•	
+        public static double BeregnArealCirkel(double radius)
+        { return (System.Math.Pow(radius, 2 * System.Math.PI)); }
+    }
+}
--- Day4_Exercise2/ProgramDay4_Exercise2.cs
+++ Day4_Exercise2/ProgramDay4_Exercise2.cs
@@ -5,13 +5,14 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace ProgramDay4_Exercise2
-{
+{//Day4_Exercise2 - Statiske metoder. Status:Done
     class ProgramDay4_Exercise2
     {
         static void Main(string[] args)
         {
+            ArealBeregningerClass inst = new ArealBeregningerClass();
             Console.WriteLine("ArealBeregningerClass: " + ArealBeregningerClass.BeregnArealFirkant(10, 10));
-            Console.WriteLine("ArealBeregningerClass: " + ArealBeregningerClass.ArealBeregningerClassArealCircle(10));
+            Console.WriteLine("ArealBeregningerClass: " + ArealBeregningerClass.BeregnArealCirkel(10));
             Console.ReadKey();
         }
     }
